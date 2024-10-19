062e8af2c51e739cbc79e914a651195d91174253
Russell Semsem <109185074+russellsemsem@users.noreply.github.com>
2024-10-18 22:46:27 -0700

    updated landing

--- frontend/src/pages/Landing.js
+++ frontend/src/pages/Landing.js
@@ -4,8 +4,7 @@ import { Link } from 'react-router-dom';
 const Landing = () => {
     return (
       <div className="landing-container">
-        <h1>Welcome to the Landing Page</h1>
-        <p>This is your starting page!</p>
+        <h1>Landing Page</h1>
       </div>
     );
   };
