6a70b6ecaddd0d2b3d13c68e17989f038ea22986
Russell Semsem <109185074+russellsemsem@users.noreply.github.com>
2024-10-18 22:31:23 -0700

    Installed react-router-dom, init landing page

---
+++ frontend/src/pages/Landing.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Landing = () => {
+    return (
+      <div className="landing-container">
+        <h1>Welcome to the Landing Page</h1>
+        <p>This is your starting page!</p>
+      </div>
+    );
+  };
+  
+  export default Landing;
