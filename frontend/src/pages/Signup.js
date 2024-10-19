efbf68d4e3619a9c929e3447addb6ab84453f12d
Russell Semsem <109185074+russellsemsem@users.noreply.github.com>
2024-10-19 04:19:15 -0700

    finished signup page

--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -6,12 +6,38 @@ import '../Styles/Universal.css';
 const Signup = () => {
     return (
       <body>
-        <div className='nav-bar'>
+        <div className='signup-nav-bar'>
             <Link to='/'><div className='logo'>Ret<span className="gradient-text">ai</span>n</div></Link>
-            <ul>
-                <li><Link to="/signup" className='nav-button'>Sign up</Link></li>
-                <li><Link to="/login" className='nav-button'>Log in</Link></li>
-            </ul>
+        </div>
+        <div className='signup-form'>
+            <form>
+                <div className="field">
+                    <input type="text" name="firstName" placeholder="First Name" required />
+                </div>
+
+                <div className="field">
+                    <input type="text" name="lastName" placeholder="Last Name" required />
+                </div>
+
+                <div className="field">
+                    <input type="email" name="email" placeholder="School Email" required />
+                </div>
+
+                <div className="field">
+                    <input type="password" name="password" placeholder="Password" required />
+                </div>
+
+                <div className="field">
+                    <input type="password" name="confirmPassword" placeholder="Confirm Password" required />
+                </div>
+
+                <div>
+                    <button type="submit">Sign Up</button>
+                </div>
+                <div className="login-link">
+                    <Link to="/login" className='nav-button'>Already have an account? Log in.</Link>
+                </div>
+            </form>
         </div>
       </body>
     );
