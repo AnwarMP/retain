ee1ca6c76b464727723de5e564510eddab0dd045
Harinandan Kotamsetti <hareee234@gmail.com>
2024-10-19 00:52:45 -0700

    header boilerplate

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,10 +4,14 @@ import './App.css';
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Landing from './pages/Landing';
+import Header from './components/Header'
 
 function App() {
   return (
     <Router>
+      <Header>
+
+      </Header>
       <div className="App">
         <Routes>
           {/* Route for the Landing Page */}
